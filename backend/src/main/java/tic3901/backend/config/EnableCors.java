d543b44a7c3d1a6b6dfbc27d85a30c71cc40a350
Zac <guangzu.wang@u.nus.edu>
2023-09-26 00:07:12 +0800

    backend add enablecors, frontend add setupProxy

---
+++ backend/src/main/java/tic3901/backend/config/EnableCors.java
@@ -0,0 +1,27 @@
+package tic3901.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class EnableCors {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from the React development server (adjust this as needed)
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
+
